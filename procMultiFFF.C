eb15c57d617ba8e6235ab3c7f797f80a9cc6bbbf
lm585 <lm585@users.noreply.github.com>
2018-03-21 23:55:18 -0400

    (F,F,F)    (R,R,R)  -> F,      R,  ALSO simplified
    read start coord group
    
    Stringstream : convert int to string
    
    Chr1    29236   29610   Chr1    29783   30151   3       (FC42KE5AAXX:7:39:1534:82#0,FC42KE5AAXX:7:40:552:2021#0,FC42KE5AAXX:7:89:1175:2011#0)   (F,F,F) (R,R,R)      (2,2,1) (1,1,2) (1,2,3) (1,2,3) (29236,29402,29567)     (29826,29972,30151)     DELETION        581.333333333333        3/3     UNBAL   3/3     3/3 (29610,29738)    (29649,29783)   1       3
    
    ======>
    Chr1    29236   29610   Chr1    29783   30151   3       (FC42KE5AAXX:7:39:1534:82#0,FC42KE5AAXX:7:40:552:2021#0,FC42KE5AAXX:7:89:1175:2011#0)   F,      R,  (2,2,1)  (1,1,2) (1,2,3) (1,2,3) (29236,29567)   (29826,30151)   DELETION        581.333333333333        3/3     UNBAL   3/3     3/3     (29610,29738)   (29649,29783)        1       3

---
+++ procMultiFFF.C
@@ -0,0 +1,227 @@
+/*
+SL2.40ch01	9006479	9006593	SL2.40ch02	7406683	7406779	653	(R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R*)	(F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F*)	TRANSLOC	"empty"	UNBAL
+
+output (R)	(F)
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <set>
+#include <vector>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+
+void getFieldContent2(vector<string> & fields, string del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del.find(line[i]) != string::npos) 
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   if(len > 0)
+   {
+    str = line.substr(pos[i] + 1, len);
+    fields.push_back(str);
+   }
+  }
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+void getFieldContent(vector<string> & fields, char del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del == line[i]) 
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   str = line.substr(pos[i] + 1, len);
+   fields.push_back(str);
+  } 
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+void parseStrand(string orig, string & res)
+{
+ string del1 = "()", del2 = ",";
+ vector<string> strVect1, strVect2;
+ res = "";
+ getFieldContent2(strVect1, del1 , orig);
+ for(int i = 0; i < strVect1.size(); i++)
+ {
+  getFieldContent2(strVect2, del2, strVect1[i]);
+  for(int j = 0; j < strVect2.size(); j++)
+  {
+   if(strVect2[j] == "F" || strVect2[j] == "R")
+   {
+    res = res + strVect2[j] + ',';
+    break;
+   }
+  }
+ }
+}
+
+void findMinMax(const vector<int> & intVect, string & min, string & max)
+{
+ int a, b;
+ stringstream ss;
+ 
+ ss.str("");
+ ss.clear();
+ a = intVect[0];
+ b = intVect[0];
+ for(int i = 1; i < intVect.size(); i++)
+ {
+  if(intVect[i] < a)
+    a = intVect[i];
+  if(intVect[i] > b)
+    b = intVect[i];
+ }
+ ss << a;
+ min = ss.str();
+ ss.str("");
+ ss.clear();
+ ss << b;
+ max = ss.str();
+}
+
+//(2556496,2556528,2556529,2556533,2557713$),(2556439,2556453,2556458)     BAL
+//(2555940,2555943,2555943)               UNBAL
+void parseStarts(string orig, string & res)
+{
+ string del1 = "()", del2 = ",";
+ vector<string> strVect1, strVect2;
+ vector<int> startVect;
+ string min, max;
+ res = "";
+ getFieldContent2(strVect1, del1 , orig);
+ for(int i = 0; i < strVect1.size(); i++) //strVect1[1] == "," for BAL
+ {
+  getFieldContent2(strVect2, del2, strVect1[i]);
+  for(int j = 0; j < strVect2.size(); j++) //2556496 2556528  2556529  2556533   2557713$
+  {
+   if(strVect2[j].find('$') == string::npos && strVect2[j].find('*') == string::npos && strVect2[j].find('@') == string::npos)
+   {
+    startVect.push_back(atoi(strVect2[j].c_str()));
+   }
+  }
+  if(startVect.size() > 0)
+  {
+   findMinMax(startVect, min, max);
+   startVect.clear();
+   res = res + "(" + min + "," + max + ")";
+  }
+ }
+}
+
+int main(int argc, char * argv[])
+{
+ if(argc != 3)
+ {
+  cerr << argv[0] << " inputFile(SVDetect filtered outout)	outputFile\n";
+  return 1;
+ }
+
+ ifstream in;
+ ofstream out;
+
+ in.open(argv[1], ios::in);
+ if(!in)
+ {
+  cerr << argv[1] << " cannot be open for reading.\n";
+  return 1;
+ }
+ out.open(argv[2], ios::out);
+ if(!out)
+ {
+  cerr << argv[2] << " cannot be open for writing.\n";
+  return 1;
+ }
+
+ string line,  strF, strR, start1, start2;
+ char del;
+ vector<string> fields;
+ 
+ del = '\t';
+ getline(in, line);
+ while(!in.eof())
+ {
+  getFieldContent(fields, del, line);
+  parseStrand(fields[8], strF);
+  parseStrand(fields[9], strR);
+  parseStarts(fields[14], start1);
+  parseStarts(fields[15], start2);
+  for(int i = 0; i < fields.size(); i++)
+  {
+   if(i == 8)
+      out << strF << '\t';
+   else if(i == 9)
+      out << strR << '\t';
+   else if(i == 14)
+     out << start1 << '\t';
+   else if(i == 15)
+     out << start2 << '\t';
+   else
+     out << fields[i] << '\t';
+  }
+  out << endl;
+  getline(in, line);
+ }
+ in.close();
+ out.close();
+ return 0;
+}
