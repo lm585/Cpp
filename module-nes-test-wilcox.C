51b25855a79cd2127f690c9e724bfeeec84e76c6
lm585 <lm585@users.noreply.github.com>
2020-09-22 16:13:05 -0400

    Add files via upload

---
+++ module-nes-test-wilcox.C
@@ -0,0 +1,590 @@
+/*
+cluster0vs1, 0vs2, 0vs9, nine compare
+...
+cluster9vs0, .., 9vs8, nine compare
+total 9 x 10 = 90 compares
+some field is 'NA', some field is empty
+
+1	HALLMARK_ALLOGRAFT_REJECTION
+2	-0.8593123
+3	0.946
+4	0.95725924
+5	HALLMARK_ALLOGRAFT_REJECTION
+6	-1.1819897
+7	0.055555556
+8	0.4075095
+9	HALLMARK_ALLOGRAFT_REJECTION
+10	-1.6805482
+11	0.0
+12	0.01582257
+13	HALLMARK_ALLOGRAFT_REJECTION
+14	-1.7981485
+15	0.0
+16	0.0058311843
+17	HALLMARK_ALLOGRAFT_REJECTION
+18	-1.8175857
+19	0.0
+20	0.0051447432
+21	HALLMARK_ALLOGRAFT_REJECTION
+22	-1.8078613
+23	0.0
+24	0.0012884389
+25	HALLMARK_ALLOGRAFT_REJECTION
+26	-1.7272013
+27	0.0
+28	0.034295205
+29	HALLMARK_ALLOGRAFT_REJECTION
+30	1.0912993
+31	0.1965812
+32	0.7494065
+33	HALLMARK_ALLOGRAFT_REJECTION
+34	-1.2011684
+35	0.035
+36	0.57764244
+37	HALLMARK_ALLOGRAFT_REJECTION
+38	0.8516667
+39	0.945
+40	0.97443545
+41	HALLMARK_ALLOGRAFT_REJECTION
+42	
+43	
+44	1.0
+45	HALLMARK_ALLOGRAFT_REJECTION
+46	-1.5321678
+47	0.0
+48	0.051593065
+49	HALLMARK_ALLOGRAFT_REJECTION
+50	-2.1744509
+51	0.0
+52	1.0635558E-4
+53	HALLMARK_ALLOGRAFT_REJECTION
+54	-2.2690122
+55	0.0
+56	0.0
+57	HALLMARK_ALLOGRAFT_REJECTION
+58	-2.2010064
+59	0.0
+60	3.659864E-5
+61	HALLMARK_ALLOGRAFT_REJECTION
+62	-1.5556377
+63	0.0
+64	0.19841978
+65	HALLMARK_ALLOGRAFT_REJECTION
+66	1.0689536
+67	0.267
+68	0.6209141
+69	HALLMARK_ALLOGRAFT_REJECTION
+70	-1.6282992
+71	0.0
+72	0.13545781
+73	HALLMARK_ALLOGRAFT_REJECTION
+74	1.151072
+75	0.12195122
+76	0.44502777
+77	HALLMARK_ALLOGRAFT_REJECTION
+78	
+79	
+80	1.0
+81	HALLMARK_ALLOGRAFT_REJECTION
+82	-1.2561452
+83	0.014
+84	0.37111345
+85	HALLMARK_ALLOGRAFT_REJECTION
+86	-1.1614743
+87	0.118
+88	0.63596886
+89	HALLMARK_ALLOGRAFT_REJECTION
+90	-1.0759758
+91	0.288
+92	0.755482
+93	HALLMARK_ALLOGRAFT_REJECTION
+94	-1.3443694
+95	0.0
+96	0.2271025
+97	HALLMARK_ALLOGRAFT_REJECTION
+98	-1.188765
+99	0.10932799
+100	0.645497
+101	HALLMARK_ALLOGRAFT_REJECTION
+102	-0.8360046
+103	0.8685541
+104	0.9775189
+105	HALLMARK_ALLOGRAFT_REJECTION
+106	-0.861599
+107	0.8406814
+108	1.0
+109	HALLMARK_ALLOGRAFT_REJECTION
+110	1.6781622
+111	0.0
+112	0.016436769
+113	HALLMARK_ALLOGRAFT_REJECTION
+114	1.5489259
+115	0.0
+116	0.052036665
+117	HALLMARK_ALLOGRAFT_REJECTION
+118	1.250858
+119	0.008
+120	0.3765374
+121	HALLMARK_ALLOGRAFT_REJECTION
+122	1.3397772
+123	0.002
+124	0.26347545
+125	HALLMARK_ALLOGRAFT_REJECTION
+126	1.2213361
+127	0.06906907
+128	0.4591583
+129	HALLMARK_ALLOGRAFT_REJECTION
+130	-1.1666822
+131	0.106796116
+132	0.72441435
+133	HALLMARK_ALLOGRAFT_REJECTION
+134	0.8471774
+135	0.896
+136	1.0
+137	HALLMARK_ALLOGRAFT_REJECTION
+138	1.7358531
+139	0.0
+140	0.003933258
+141	HALLMARK_ALLOGRAFT_REJECTION
+142	2.023261
+143	0.0
+144	3.8617986E-4
+145	HALLMARK_ALLOGRAFT_REJECTION
+146	1.7873409
+147	0.0
+148	0.008724282
+149	HALLMARK_ALLOGRAFT_REJECTION
+150	2.1844976
+151	0.0
+152	1.113622E-4
+153	HALLMARK_ALLOGRAFT_REJECTION
+154	1.1755532
+155	0.0990991
+156	0.62461495
+157	HALLMARK_ALLOGRAFT_REJECTION
+158	-1.358901
+159	0.001001001
+160	0.25217056
+161	HALLMARK_ALLOGRAFT_REJECTION
+162	-1.6352522
+163	0.0
+164	0.14523496
+165	HALLMARK_ALLOGRAFT_REJECTION
+166	-1.8193778
+167	0.0
+168	0.011813264
+169	HALLMARK_ALLOGRAFT_REJECTION
+170	0.9322187
+171	0.64126986
+172	0.883879
+173	HALLMARK_ALLOGRAFT_REJECTION
+174	1.8657689
+175	0.0
+176	8.026059E-4
+177	HALLMARK_ALLOGRAFT_REJECTION
+178	1.8592526
+179	0.0
+180	0.06676347
+181	HALLMARK_ALLOGRAFT_REJECTION
+182	1.8013332
+183	0.0
+184	0.009839821
+185	HALLMARK_ALLOGRAFT_REJECTION
+186	2.2243373
+187	0.0
+188	0.0
+189	HALLMARK_ALLOGRAFT_REJECTION
+190	1.0677043
+191	0.31
+192	0.77890015
+193	HALLMARK_ALLOGRAFT_REJECTION
+194	-1.2073694
+195	0.08908909
+196	0.47980225
+197	HALLMARK_ALLOGRAFT_REJECTION
+198	1.6174042
+199	0.0
+200	0.17718089
+201	HALLMARK_ALLOGRAFT_REJECTION
+202	-1.5775526
+203	0.0
+204	0.25849214
+205	HALLMARK_ALLOGRAFT_REJECTION
+206	0.9486323
+207	0.65531063
+208	0.8750432
+209	HALLMARK_ALLOGRAFT_REJECTION
+210	1.8022548
+211	0.0
+212	0.0013412255
+213	HALLMARK_ALLOGRAFT_REJECTION
+214	2.0580647
+215	0.0
+216	0.0026588074
+217	HALLMARK_ALLOGRAFT_REJECTION
+218	1.8158135
+219	0.0
+220	0.0011699983
+221	HALLMARK_ALLOGRAFT_REJECTION
+222	2.1832464
+223	0.0
+224	1.8967307E-4
+225	HALLMARK_ALLOGRAFT_REJECTION
+226	1.3561552
+227	0.001
+228	0.2153347
+229	HALLMARK_ALLOGRAFT_REJECTION
+230	1.1804492
+231	0.10215054
+232	0.6936374
+233	HALLMARK_ALLOGRAFT_REJECTION
+234	1.8033777
+235	0.0
+236	0.01489906
+237	HALLMARK_ALLOGRAFT_REJECTION
+238	1.5957195
+239	0.0
+240	0.22602879
+241	HALLMARK_ALLOGRAFT_REJECTION
+242	1.3396944
+243	0.004016064
+244	0.29002896
+245	HALLMARK_ALLOGRAFT_REJECTION
+246	1.9815005
+247	0.0
+248	1.10790315E-4
+249	HALLMARK_ALLOGRAFT_REJECTION
+250	2.003
+251	0.0
+252	0.0035431315
+253	HALLMARK_ALLOGRAFT_REJECTION
+254	1.7192984
+255	0.0
+256	0.037229784
+257	HALLMARK_ALLOGRAFT_REJECTION
+258	1.5516464
+259	0.0
+260	0.2046486
+261	HALLMARK_ALLOGRAFT_REJECTION
+262	1.1809736
+263	0.12273642
+264	0.66484475
+265	HALLMARK_ALLOGRAFT_REJECTION
+266	-0.8555641
+267	0.872
+268	1.0
+269	HALLMARK_ALLOGRAFT_REJECTION
+270	-0.9270679
+271	0.64576805
+272	0.8734811
+273	HALLMARK_ALLOGRAFT_REJECTION
+274	-0.9580368
+275	0.63627255
+276	0.8675306
+277	HALLMARK_ALLOGRAFT_REJECTION
+278	-1.3472787
+279	0.009018037
+280	0.28316474
+281	HALLMARK_ALLOGRAFT_REJECTION
+282	1.5388538
+283	0.0
+284	0.1183299
+285	HALLMARK_ALLOGRAFT_REJECTION
+286	1.401411
+287	0.0
+288	0.30672652
+289	HALLMARK_ALLOGRAFT_REJECTION
+290	-1.1031158
+291	0.19444445
+292	0.7474548
+293	HALLMARK_ALLOGRAFT_REJECTION
+294	-1.0731732
+295	0.249
+296	0.61076635
+297	HALLMARK_ALLOGRAFT_REJECTION
+298	0.8372602
+299	0.8670695
+300	0.9846551
+301	HALLMARK_ALLOGRAFT_REJECTION
+302	-1.7312372
+303	0.0
+304	0.0034746134
+305	HALLMARK_ALLOGRAFT_REJECTION
+306	-1.8449105
+307	0.0
+308	4.9848045E-4
+309	HALLMARK_ALLOGRAFT_REJECTION
+310	-1.7993534
+311	0.0
+312	8.4566924E-4
+313	HALLMARK_ALLOGRAFT_REJECTION
+314	-1.9493277
+315	0.0
+316	1.01767204E-4
+317	HALLMARK_ALLOGRAFT_REJECTION
+318	-1.557094
+319	0.0
+320	0.108314894
+321	HALLMARK_ALLOGRAFT_REJECTION
+322	-1.3729489
+323	0.005005005
+324	0.32770848
+325	HALLMARK_ALLOGRAFT_REJECTION
+326	1.1998714
+327	0.039
+328	0.5784344
+329	HALLMARK_ALLOGRAFT_REJECTION
+330	1.6085838
+331	0.0
+332	0.15621233
+333	HALLMARK_ALLOGRAFT_REJECTION
+334	0.8657168
+335	0.85055166
+336	1.0
+337	HALLMARK_ALLOGRAFT_REJECTION
+338	-1.9909474
+339	0.0
+340	9.038774E-4
+341	HALLMARK_ALLOGRAFT_REJECTION
+342	-1.8163909
+343	0.0
+344	0.10083558
+345	HALLMARK_ALLOGRAFT_REJECTION
+346	-2.0502105
+347	0.0
+348	0.0011322054
+349	HALLMARK_ALLOGRAFT_REJECTION
+350	-1.9821268
+351	0.0
+352	0.0045567187
+353	HALLMARK_ALLOGRAFT_REJECTION
+354	-1.3847955
+355	0.0
+356	0.3188879
+357	HALLMARK_ALLOGRAFT_REJECTION
+358	1.3843042
+359	0.0020140987
+360	0.32178646
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+void readFile(const char * argv, ifstream & input);
+void writeFile(const char * argv, ofstream & output);
+void getFieldContent(vector<string> & fields, char del, string line);
+void getFieldContent2(vector<string> & fields, string del, string line);
+
+int main(int argc, char *argv[])
+{
+ if(argc != 4)
+ {
+  cerr << argv[0] << "module-nes-p-q-file(multi line, each line a pathway)	 10(# of clusters)	9(# of compares per cluster)" << endl;
+  return 1;
+ }
+
+ ifstream input;
+ ofstream output;
+ string line;
+ vector<string> lineFields;
+ vector< vector<double> > queryClust, otherClusters;
+ int numOfClust = atoi(argv[2]);
+ int compPerClust = atoi(argv[3]);
+
+ vector<double> tempVectDoub;
+ for(int i = 1; i <= numOfClust; i++)
+ {
+  queryClust.push_back(tempVectDoub);
+  otherClusters.push_back(tempVectDoub);
+ }
+
+ readFile(argv[1], input);
+ getline(input, line);
+ while(!input.eof())
+ {
+  getFieldContent(lineFields, '\t', line);
+  if(lineFields.size() != numOfClust * compPerClust * 4)
+  {
+   cerr << lineFields[0] << " != " << numOfClust * compPerClust * 4 << " fields" << endl;
+   return 1;
+  }
+  //each cluster,
+  //a is 9-NES of the cluster, b is 9*9 = 81 NES of other 9 clusters
+  // a <- c(-2, 0, -1, 1, -2)
+  // b <- c(-1, 1, -1, 1, -1, 1, -1, 1)
+  // r <- wilcox.test(-a,b, exact=F, conf.int = T)
+  // r$p.value
+  // r$estimate
+  //some field is 'NA' or empty
+  //if a is empty or only 1 NES, then no wilcox test, p = 1, estimat = 0
+  for(int i = 0; i < numOfClust; i++) //0,1,2,..,9
+  {
+    //cluster 3 (i == 2); (i) * compPerClust * 4 (2*9*4 = 72), 
+    //start from field #73, end #108
+    //(i+1) * compPerClust * 4 + 1(3*9*4 + 1= 108 +1 = 109)
+   for(int j = 1; j < lineFields.size(); j = j+4)
+   {
+    if(j > (i) * compPerClust * 4 && j < (i+1) * compPerClust * 4 + 1)
+    {
+     if(lineFields[j] != "" && lineFields[j] != "NA")
+     	queryClust[i].push_back(atof(lineFields[j].c_str()));
+    }
+    else
+    {
+     if(lineFields[j] != "" && lineFields[j] != "NA")
+       otherClusters[i].push_back(atof(lineFields[j].c_str()));
+    }
+   }
+  } //end of for(int i = 1; i <= numOfClust; i++)
+  getline(input, line);
+ }
+ input.close();
+ 
+ for(int i = 0; i < numOfClust; i++) //0,1,2,..,9
+ {
+  cout << "a <- c(";
+  for(int j = 0; j < queryClust[i].size(); j++)
+  {
+    if(j != queryClust[i].size() -1)
+      cout << queryClust[i][j] << ", ";
+    else
+      cout << queryClust[i][j] ;
+  }
+  cout << ")" << endl;
+  
+  cout << "b <- c(";
+  for(int j = 0; j < otherClusters[i].size(); j++)
+  {  
+    if(j != otherClusters[i].size() -1)
+      cout << otherClusters[i][j] << ", ";
+    else
+      cout << otherClusters[i][j] ;
+  }
+  cout << ")" << endl;
+  if(queryClust[i].size() <= 1)
+  {
+   cout << "print(\"[1] 1\")" << endl;
+   cout << "print(\"difference in location \")" << endl;
+   cout << "print(\"             0 \")" << endl;
+  }
+  else
+  {
+   cout << "r <- wilcox.test(a,b, exact=F, conf.int = T)" << endl;
+   cout << "r$p.value" << endl;
+   cout << "r$estimate" << endl;
+  }
+ }
+
+ //writeFile(argv[99], output);
+ //output.close();
+ return 0;
+}
+
+void readFile(const char * argv, ifstream & input)
+{
+ input.open(argv, ios::in);
+ if(!input)
+ {
+  cerr << argv << " can not be opened for reading.\n";
+  exit(1);
+ }
+}
+
+void writeFile(const char * argv, ofstream & output)
+{
+ output.open(argv, ios::out);
+ if(!output)
+ {
+  cerr << argv << " can not be opened for writing.\n";
+  exit(1);
+ }
+}
+
+void getFieldContent(vector<string> & fields, char del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del == line[i])
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   str = line.substr(pos[i] + 1, len);
+   fields.push_back(str);
+  }
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+void getFieldContent2(vector<string> & fields, string del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del.find(line[i]) != string::npos)
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   if(len > 0)
+   {
+    str = line.substr(pos[i] + 1, len);
+    fields.push_back(str);
+   }
+  }
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+
